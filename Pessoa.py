ae90a02a00de2980272e7003cfa7b75151626b54
Vinicius Silva de Oliveira <115173332+kyuubiV@users.noreply.github.com>
2022-11-11 20:56:50 +0000

    atulizacao da classe pessoa

--- Pessoa.py
+++ Pessoa.py
@@ -2,28 +2,13 @@ from ConexaoBD import *
 from Banco import *
 user='a definir'
 class Pessoa:
-    def __init__(self):
-        self._email = None
-        self._senha = None
-        self._telefone = None
+    def __init__(self,email,senha,telefone):
+        self._email = email
+        self._senha = senha
+        self._telefone = telefone
         self._dict = {}   
         self._nm_user = user
         self._dataniver = user
-# atributos usuario
-    def AddUsuario(self):
-        self._email  = input("Insira o email: ")
-        self._senha= input("Insira a senha: ")
-        self._telefone = input("Insira o telefone: ")
-    def ConfirmaDado(self):
-        x= input("Confirme o seu email: ")
-        y= input("Confirme a sua senha: ")
-        if self._email == x and self._senha == y:
-            print("\033[0;49;32mTudo certo!")
-            return 's'
-        else:
-            print("\033[0;49;31merro os dados digitados não coincidem")
-            print('\033[1;94m')
-            return 'n'
 # metodo para confirmar dados        
     def AddDados(self):
       try:
